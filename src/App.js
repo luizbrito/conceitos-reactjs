00d05167514ede1d41ff627ac1da53d98cf900b7
Luiz Brito da Rosa <luiz.rosa@logicinfo.com>
2020-09-26 13:17:14 -0300

    Adicionada Funcionalidade de delete.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+
+  const[repositories, setRepositories] = useState([]);
+  
+
+  async function loadRepositories() {
+    await api.get(`/repositories`).then( response => {
+      console.log(response);
+      setRepositories(response.data);
+    });
+  }
+  
+  useEffect( () => {
+    loadRepositories();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post(`/repositories`, {      
+      url: "https://github.com/luizbrito",
+      title: "Testes",
+      techs: ["React", "Node.js"],
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+    
+    setRepositories(repositories.filter(repository => repository.id !== id));
+
   }
 
   return (
     <div>
+      
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repo =>
+          <li key={repo.id}>
+            {repo.title}   
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>)
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
+      
     </div>
   );
 }
